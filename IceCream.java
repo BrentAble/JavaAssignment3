e6c195efe9f2e8a52c51b008c95575090fba6a5f
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-09-23 08:16:25 -0400

    Update IceCream.java

--- IceCream.java
+++ IceCream.java
@@ -3,10 +3,14 @@
  */
 public abstract class IceCream {
 	protected String iceCream;
-	
+	/*
+	* Returns the ice cream desired
+	*/
 	public String toStringn() {
 		return iceCream;
 	}
-	
+	/*
+	* Returns all cost values and adds them up
+	*/
 	public abstract double getCost();
 }
