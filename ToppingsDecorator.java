9b9601710f658fde7a701e38a8e29fd3474ce7f9
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-09-23 08:21:11 -0400

    Update ToppingsDecorator.java

--- ToppingsDecorator.java
+++ ToppingsDecorator.java
@@ -3,4 +3,7 @@
  */
 public abstract class ToppingsDecorator extends IceCream {
 	public abstract String toString();
+	/*
+	* Creates an abstract string method that will filter down to toppings classes
+	*/
 }
