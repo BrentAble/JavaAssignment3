1af749894ce6b5ccb713677f469b3d4f68cdbfcd
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-09-23 08:23:06 -0400

    Update VanillaIceCream.java

--- VanillaIceCream.java
+++ VanillaIceCream.java
@@ -2,11 +2,15 @@
  * Author @ Brent Able
  */
 public class VanillaIceCream extends IceCream {
-	
+	/*
+	* sets ice cream and string output if chosen
+	*/
 	public VanillaIceCream() {
 		this.iceCream = "Classic Ice Cream";
 	}
-	
+	/*
+	* Get costs sets price of this ice cream
+	*/
 	public double getCost() {
 		return 1.25;
 	}
