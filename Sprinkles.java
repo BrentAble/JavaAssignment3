57fa086b3ac3b73fb485432ba0d5cf47e1cede08
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-09-23 08:18:59 -0400

    Update Sprinkles.java

--- Sprinkles.java
+++ Sprinkles.java
@@ -3,15 +3,21 @@
  */
 public class Sprinkles extends ToppingsDecorator {
 	IceCream iceCream;
-	
+	/*
+	* Adds chosen icecream from Class IceCream as type
+	*/
 	public Sprinkles(IceCream iceCream) {
 		this.iceCream = iceCream;
 	}
-	
+	/*
+	* Adds string for if sprinkles are selected
+ 	*/
 	public String toString() {
 		return ", Rainbow Sprinkles";
 	}
-	
+	/*
+	* Gets cost of sprinkles and adds it to whatever chosen ice cream
+	*/
 	public double getCost() {
 		return iceCream.getCost() + 0.25;
 	}
