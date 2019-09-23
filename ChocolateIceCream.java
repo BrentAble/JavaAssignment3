505ed9dfe0a39dde0024310cc2eb6d21a135d300
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-09-23 08:15:35 -0400

    Update ChocolateIceCream.java

--- ChocolateIceCream.java
+++ ChocolateIceCream.java
@@ -2,11 +2,15 @@
  * Author @ Brent Able
  */
 public class ChocolateIceCream extends IceCream {
-	
+	/*
+	* Sets Ice cream and string for if chosen
+	*/
 	public ChocolateIceCream() {
 		this.iceCream = "Creamy Choclate";
 	}
-	
+	/*
+	* Get Cost sets price of ice cream
+	*/
 	public double getCost() {
 		return 1.50;
 	}
