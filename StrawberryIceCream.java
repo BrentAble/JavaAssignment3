824b164949dd04258990ea95186093dd11234c01
BrentAble <43350759+BrentAble@users.noreply.github.com>
2019-09-23 08:20:19 -0400

    Update StrawberryIceCream.java

--- StrawberryIceCream.java
+++ StrawberryIceCream.java
@@ -2,11 +2,15 @@
  * Author @ Brent Able
  */
 public class StrawberryIceCream extends IceCream {
-	
+	/*
+	* Sets ice cream and string for if chosen
+	*/ 
 	public StrawberryIceCream() {
 		this.iceCream = "Pink colored Ice Cream";
 	}
-	
+	/*
+	* Gets cost sets price of the ice cream
+	*/
 	public double getCost() {
 		return 1.50;
 	}
